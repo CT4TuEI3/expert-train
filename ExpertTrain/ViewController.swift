df9a1a604555b2db0d0291c0e59a1cb3f05c70a6
CT4TuEI3 <alexstatiev@gmail.com>
2025-06-09 20:41:25 +0300

    Initial Commit

---
+++ ExpertTrain/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ExpertTrain
+//
+//  Created by CT4TuEI3 on 09.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+    
+    
+}
